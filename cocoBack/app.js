629286b731870f02f5630de84306d95378543e39
J1Loop <jorge.ua.1996@gmail.com>
2022-07-15 22:55:36 +0200

    app.js -> - http-errors require + 2 app.use comentados con error - bcrypt instalado para encriptar passwords - request de registro hecha, validación en username email y password

--- cocoBack/app.js
+++ cocoBack/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
@@ -21,5 +22,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/usuarios', usuariosRouter);
 app.use('/api', apiRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: err
+    });
+});
 
 module.exports = app;
--- cocoBack/routes/api/usuarios.js
+++ cocoBack/routes/api/usuarios.js
@@ -1,7 +1,50 @@
 const router = require('express').Router();
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const Usuario = require('../../models/usuario.model');
+
+router.get('/', async (req, res) => {
+    try {
+        const result = await Usuario.getAll();
+        res.json(result);
+    } catch (err) {
+        res.json({ msg: err.message, error: err })
+    };
+});
+
+router.post('/registro',
+    body('username')
+        .exists()
+        .withMessage('El campo de username es obligatorio.')
+        .isLength({ min: 4, max: 15 })
+        .withMessage('El campo username debe tener una longitud entre x y z caracteres.'),
+    body('email')
+        .isEmail()
+        .withMessage('El formato del email es incorrecto.'),
+    body('password')
+        .exists()
+        .withMessage('Debes introducir una contraseña.')
+    , async (req, res) => {
+        // Comprobar errores de las validaciones.
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.json(errors.array());
+        };
+
+        // require bcrypt para encriptar la password
+        // try encriptado + creación del usuario
+        try {
+            req.body.password = bcrypt.hashSync(req.body.password, 13);
+            const result = await Usuario.create(req.body);
+            console.log(result);
+            res.json(result);
+        } catch (err) {
+            res.json({ error: err.message });
+        };
 
 
 
-
+    }
+);
 
 module.exports = router;
