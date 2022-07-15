f43f5d303fc7a1d1080512528bf0e5ba242421f4
J1Loop <jorge.ua.1996@gmail.com>
2022-07-15 19:17:27 +0200

    app.js y rutas **APP.JS** - require y use del cors, rename de ruta users -> usuarios - ruteo api linkeado **RUTAS** - Creación de api api/usuarios api/productos - Middleware a api/productos - Inicio routes api/productos+usuarios

--- cocoBack/app.js
+++ cocoBack/app.js
@@ -1,13 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usuariosRouter = require('./routes/usuarios');
+const apiRouter = require('./routes/api');
 
-var app = express();
+const app = express();
 
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,6 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/usuarios', usuariosRouter);
+app.use('/api', apiRouter);
 
 module.exports = app;
