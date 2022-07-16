9b95180b9329b8fc9dff3f5151459ee2aa829665
J1Loop <jorge.ua.1996@gmail.com>
2022-07-16 23:52:27 +0200

    CRUD USUARIOS, mañana más.

--- cocoBack/models/usuario.model.js
+++ cocoBack/models/usuario.model.js
@@ -9,10 +9,18 @@ const getById = (id) => {
 };
 
 const getByUsername = (username) => {
-    return executeQueryOne(`SELECT * FROM usuarios WHERE username = ?`, [username])
+    return executeQuery(`SELECT * FROM usuarios WHERE username LIKE ?`, [`%${username}%`])
 };
 
+const getStrictUsername = (username) => {
+    return executeQueryOne(`SELECT * FROM usuarios WHERE username = ?`, [username])
+}
+
 const getByEmail = (email) => {
+    return executeQuery(`SELECT * FROM usuarios WHERE email LIKE ?`, [`%${email}%`]);
+};
+
+const getStrictEmail = (email) => {
     return executeQueryOne(`SELECT * FROM usuarios WHERE email = ?`, [email]);
 };
 
@@ -20,16 +28,28 @@ const getByNombre = (nombre) => {
     return executeQuery(`SELECT * FROM usuarios WHERE nombre LIKE ?`, [`%${nombre}%`])
 };
 
-const create = ({ username, email, password, direccion, nombre, apellidos, fecha_nacimiento, trusted }) => {
+const getByTrust = (trusted) => {
+    return executeQuery(`SELECT * FROM usuarios WHERE trusted = ?`, [trusted])
+}
+
+const create = ({ username, email, password, direccion, nombre, apellidos, fecha_nacimiento }) => {
     return executeQuery(
         `INSERT INTO usuarios (username, email, password, direccion, nombre, apellidos, fecha_nacimiento, trusted)
-         VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-        [username, email, password, direccion, nombre, apellidos, fecha_nacimiento, trusted]);
+         VALUES (?, ?, ?, ?, ?, ?, ?, 0)`,
+        [username, email, password, direccion, nombre, apellidos, fecha_nacimiento]);
 };
 
-const deleteById = (clienteId) => {
-    return executeQueryOne(`DELETE FROM usuarios WHERE id = ?`, [clienteId]);
+const update = (userId, { direccion, nombre, apellidos, fecha_nacimiento }) => {
+    return executeQuery(`UPDATE usuarios SET direccion = ?, nombre = ?, apellidos = ?, fecha_nacimiento = ? WHERE id = ?`, [direccion, nombre, apellidos, fecha_nacimiento, userId])
+}
+
+const updateLoginInfo = (userId, { username, email, password }) => {
+    return executeQuery(`UPDATE usuarios SET username = ?, email = ?, password = ? WHERE id = ?`, [username, email, password, userId])
+}
+
+const deleteById = (userId) => {
+    return executeQueryOne(`DELETE FROM usuarios WHERE id = ?`, [userId]);
 }
 
 
-module.exports = { getAll, getById, getByUsername, getByEmail, getByNombre, create, deleteById };
+module.exports = { getAll, getById, getByUsername, getStrictUsername, getByEmail, getStrictEmail, getByNombre, getByTrust, create, update, updateLoginInfo, deleteById };
--- cocoBack/routes/api/usuarios.js
+++ cocoBack/routes/api/usuarios.js
@@ -13,13 +13,55 @@ router.get('/', async (req, res) => {
     };
 });
 
-router.get('/:nombre', async (req, res) => {
+router.get('/:id', async (req, res) => {
+    try {
+        const result = await Usuario.getById(req.params.id);
+        res.json(result);
+    } catch (err) {
+        res.json({ msg: err.message, error: err })
+    };
+});
+
+router.get('/username/:username', async (req, res) => {
+    try {
+        const result = await Usuario.getByUsername(req.params.username);
+        res.json(result);
+    } catch (err) {
+        res.json({ msg: err.message, error: err });
+    };
+});
+
+router.get('/email/:email', async (req, res) => {
+    try {
+        const result = await Usuario.getByEmail(req.params.email);
+        res.json(result);
+    } catch (err) {
+        res.json({ msg: err.message, error: err });
+    };
+});
+
+router.get('/nombre/:nombre', async (req, res) => {
     try {
         const result = await Usuario.getByNombre(req.params.nombre);
         res.json(result)
     } catch (err) {
         res.json({ msg: err.message, error: err })
-    }
+    };
+});
+
+router.get('/trust/:trust', async (req, res) => {
+    // type of req.params es STRING
+    // el if rechaza TODO lo que no sean trusts aceptados en la DB.
+    if (req.params.trust !== '1' && req.params.trust !== '0') {
+        return res.json({ error: 'Trust debe ser 1=true o 0=false.' });
+    };
+
+    try {
+        const result = await Usuario.getByTrust(req.params.trust);
+        res.json(result);
+    } catch (err) {
+        res.json({ msg: err.message, error: err });
+    };
 });
 
 router.post('/registro',
@@ -27,7 +69,7 @@ router.post('/registro',
         .exists()
         .withMessage('El campo de username es obligatorio.')
         .isLength({ min: 4, max: 15 })
-        .withMessage('El campo username debe tener una longitud entre x y z caracteres.'),
+        .withMessage('El campo username debe tener una longitud entre 4 y 15 caracteres.'),
     body('email')
         .isEmail()
         .withMessage('El formato del email es incorrecto.'),
@@ -58,12 +100,12 @@ router.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     // Chequeamos en la DB si el user existe.
-    const user = await Usuario.getByUsername(username);
+    const user = await Usuario.getStrictUsername(username);
     if (!user) {
         return res.json({ error: 'Username y/o contraseña incorrectos. USER ERROR' });
     };
 
-    // Comparamos la password hasheada con la de la db con el bcrypt method bcrypt.compareSync
+    // Comparamos la password hasheada con la de la db con el bcrypt.compareSync
     const samePass = bcrypt.compareSync(password, user.password);
     if (!samePass) {
         return res.json({ error: 'Username y/o contraseña incorrectos. PASS ERROR' });
@@ -75,6 +117,60 @@ router.post('/login', async (req, res) => {
         success: 'Login correcto.',
         token: createToken(user)
     });
+});
+
+router.patch('/editar/:userId/user-info', async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const result = await Usuario.update(userId, req.body);
+        res.json(result);
+    } catch (err) {
+        res.json({ msg: err.message, error: err })
+    }
+});
+
+router.patch('/editar/:userId/login-info', async (req, res) => {
+    const { userId } = req.params;
+    const user = await Usuario.getById(userId);
+    const { username, email } = req.body;
+    let { password } = req.body;
+
+
+    const userCheck = await Usuario.getStrictUsername(username);
+    console.log(userCheck);
+    console.log(username);
+    if (username !== user.username && userCheck) return res.json({ error: 'El usuario ya existe' });
+
+    const emailCheck = await Usuario.getStrictEmail(email);
+    console.log(email);
+    console.log(emailCheck);
+    if (email !== user.email && emailCheck) return res.json({ error: 'El email ya existe' });
+
+    const passCheck = bcrypt.compareSync(password, user.password);
+
+    if (passCheck) {
+        console.log('passCheck okay, misma password, no la cambio');
+        try {
+            const result = await Usuario.updateLoginInfo(userId, req.body);
+            res.json(result);
+        } catch (err) {
+            res.json({ msg: err.message, error: err });
+        };
+    } else {
+        console.log(user.password, password);
+        console.log('passCheck else, CAMBIO la password');
+        password = bcrypt.hashSync(password, 13);
+        console.log(user.password, password);
+
+        try {
+            const result = await Usuario.updateLoginInfo(userId, req.body);
+            res.json(result);
+        } catch (err) {
+            res.json({ msg: err.message, error: err });
+        };
+
+    };
+    console.log(user.password);
 });
 
 module.exports = router;
