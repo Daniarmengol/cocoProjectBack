d4b36e4d272f4078e1a7de9c899178dc5304f7dc
Dani Fernández Armengol <danielito486@gmail.com>
2022-07-21 12:15:04 +0200

    creado modelo de coleccion y colecciones en api, hay que seguir con ello

--- cocoBack/routes/api.js
+++ cocoBack/routes/api.js
@@ -3,8 +3,10 @@ const router = require('express').Router();
 
 const apiUsuariosRouter = require('./api/usuarios');
 const apiProductosRouter = require('./api/productos');
+const apiColeccionesRouter = require('./api/colecciones.js');
 
 router.use('/usuarios', apiUsuariosRouter);
 router.use('/productos', apiProductosRouter);
+router.use('/colecciones', apiColeccionesRouter);
 
 module.exports = router;
