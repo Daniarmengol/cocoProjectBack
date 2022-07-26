d058818300d7ee9f164ad2d0975489dfd2e4fc29
Dani Fernández Armengol <danielito486@gmail.com>
2022-07-26 23:09:21 +0200

    agregadas peticiones que recojen datos de usuario necesarios para plasmar en la card de colecciones

--- cocoBack/models/coleccion.model.js
+++ cocoBack/models/coleccion.model.js
@@ -4,8 +4,12 @@ const getAll = () => {
     return executeQuery(`SELECT * FROM colecciones`)
 }
 
+const getLastCollections = () => {
+    return executeQuery(`SELECT group_concat(colecciones.producto_id SEPARATOR '/') AS id_productos_coleccion, colecciones.*, usuarios.* FROM colecciones, usuarios WHERE colecciones.usuario_id = usuarios.id AND colecciones.usuario_id = usuarios.id GROUP BY codigo ORDER BY fecha_creacion DESC LIMIT 6 `)
+}
+
 const getById = (id) => {
-    return executeQueryOne(`SELECT * FROM colecciones WHERE id = ? AND `, [id])
+    return executeQueryOne(`SELECT * FROM colecciones WHERE id = ?`, [id])
 }
 
 const getByCategoria = (categoria) => {
@@ -20,15 +24,18 @@ const getCollectionByUserId = (id) => {
     return executeQuery(`SELECT group_concat(colecciones.producto_id SEPARATOR '/') AS id_productos_coleccion, colecciones.*, usuarios.* FROM colecciones, usuarios WHERE colecciones.usuario_id = usuarios.id AND colecciones.usuario_id = ? GROUP BY codigo`, [id])
 }
 
+//Comprobar primero que el usuario_id de la coleeción coincide con el usuario logueado del token
 const deleteById = (id) => {
     return executeQuery(`DELETE FROM productos WHERE id = ?`, [id])
 }
 
+// El usuario_id no debe pasarse por body, debe sacarse del token
 const create = ({ nombre, categoria, imagen = `https://imgur.com/OdaDtxa`, usuario_id, fecha_creacion }) => {
     return executeQuery(`INSERT INTO productos (nombre, precio, categoria, imagen, marca, estado, usuario_id)
     VALUES (?,?,?,?,?,?,?);`, [nombre, precio, categoria, imagen, usuario_id, fecha_creacion]);
 }
 
+//Comprobar primero que el usuario_id de la coleeción coincide con el usuario logueado del token
 const edit = (id, { nombre, precio, categoria, imagen, marca, estado }) => {
 
     return executeQuery(`UPDATE productos SET nombre = ?, categoria = ?, imagen = ?, estado = ? WHERE id = ?`, [nombre, precio, categoria, imagen, marca, estado, id])
@@ -38,4 +45,4 @@ const edit = (id, { nombre, precio, categoria, imagen, marca, estado }) => {
 
 
 
-module.exports = { getAll, getById, getByCategoria, getByNombre, deleteById, create, edit, getCollectionByUserId }
+module.exports = { getAll, getById, getByCategoria, getByNombre, deleteById, create, edit, getCollectionByUserId, getLastCollections }
